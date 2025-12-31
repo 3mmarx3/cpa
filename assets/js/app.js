b3a8966a680c04445f4bab4201d74ba6430f5212
Ammar Ahmed Mostafa <ammarx3@icloud.com>
2025-12-31 19:40:37 +0200

    ED

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,41 @@
-const targetURL = "https://www.google.com/";
-const $ = id => document.getElementById(id);
-let step = 1, time = 60;
 
-const timer = setInterval(() => {
-    time--;
-    if (time <= 0) {
-        clearInterval(timer);
-        location.href = targetURL;
-    }
-    if ($("time-left")) $("time-left").innerText = time;
-    if ($("time-left-header")) $("time-left-header").innerText = time;
-}, 1000);
+        window.onload = () => {
+            const targetURL = "https://www.google.com/";
+            const $ = id => document.getElementById(id);
+            let step = 1, time = 60;
 
-document.querySelectorAll('.select-trigger').forEach(b => {
-    b.onclick = () => {
-        setTimeout(() => {
-            $(`phase-${step}`).style.display = 'none';
-            step = b.dataset.jump;
-            $(`phase-${step}`).style.display = 'block';
-            
-            if (step == 4) {
-                clearInterval(timer);
-                $('top-timer-section').style.visibility = 'hidden';
-                $('main-container').classList.add('main-container-bonus');
-                
-                confetti({
-                    particleCount: 150,
-                    spread: 100,
-                    origin: { y: 0.6 },
-                    ticks: 300
-                });
-            }
-        }, 200);
-    };
-});
+            const timer = setInterval(() => {
+                time--;
+                if (time <= 0) {
+                    clearInterval(timer);
+                    location.href = targetURL;
+                }
+                if ($("time-left")) $("time-left").innerText = time;
+                if ($("time-left-header")) $("time-left-header").innerText = time;
+            }, 1000);
 
-$('submit-action-btn').onclick = () => {
-    location.href = targetURL;
-};
+            document.querySelectorAll('.select-trigger').forEach(b => {
+                b.onclick = () => {
+                    setTimeout(() => {
+                        $(`phase-${step}`).style.display = 'none';
+                        step = b.dataset.jump;
+                        $(`phase-${step}`).style.display = 'block';
+                        
+                        if (step == 4) {
+                            $('top-timer-section').style.visibility = 'hidden';
+                            $('main-container').classList.add('main-container-bonus');
+                            
+                            confetti({
+                                particleCount: 200,
+                                spread: 100,
+                                origin: { y: 0.6 }
+                            });
+                        }
+                    }, 200);
+                };
+            });
+
+            $('submit-action-btn').onclick = () => {
+                location.href = targetURL;
+            };
+        };
